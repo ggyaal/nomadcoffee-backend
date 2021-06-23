80d4a5c358c8461127f30f029db50afbcbc6bf41
ggyaal <ggyaal@gmail.com>
2021-06-24 02:34:28 +0900

    fixed seeCoffeeShops

--- src/coffeeShops/seeCoffeeShops/seeCoffeeShops.resolvers.js
+++ src/coffeeShops/seeCoffeeShops/seeCoffeeShops.resolvers.js
@@ -1,10 +1,9 @@
 export default {
   Query: {
-    seeCoffeeShops: (_, { offset }, { client }) => ({
-      coffeeShops: client.coffeeShop.findMany({
+    seeCoffeeShops: (_, { offset }, { client }) =>
+      client.coffeeShop.findMany({
         take: 2,
         ...(offset && { skip: offset }),
       }),
-    }),
   },
 };
